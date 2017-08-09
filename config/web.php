c018636aadb54713ca0a863730f9aeca569e709a
Jevgenij Volosatov <fformula@gmail.com>
2017-08-09 22:31:01 +0300

    add Bootstrap Nav

--- config/web.php
+++ config/web.php
@@ -2,10 +2,10 @@
     return [
         'id' => 'school',
         'basePath' => realpath(__DIR__ . '/../'),
-        'defaultRoute' => 'home/index'
-//        'components' => [
-//            'request' => [
-//                'cookieValidationKey' => 'super secret code'
-//            ],
-//        ]
+        'defaultRoute' => 'home/index',
+        'components' => [
+            'request' => [
+                'cookieValidationKey' => 'super secret code'
+            ],
+        ]
     ];
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,5 +1,40 @@
-<html>
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= $this->title ?></title>
+        <?php $this->head() ?>
+
+    </head>
     <body>
+<?php $this->beginBody() ?>
+
+<?php
+    NavBar::begin([
+            'brandLabel' => 'VideoSchool',
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar-inverse navbar-fixed-top'
+            ]
+        ]);
+    NavBar::end(); ?>
+
+
         <?= $content ?>
+
+<?php $this->endBody() ?>
+
     </body>
 </html>
+<?php $this->endPage() ?>
+
