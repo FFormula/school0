5eabb511234400520448432e7a7289c8a83f88b8
Jevgenij Volosatov <fformula@gmail.com>
2017-08-09 21:52:34 +0300

    controller and view

---
+++ config/web.php
@@ -0,0 +1,11 @@
+<?php
+    return [
+        'id' => 'school',
+        'basePath' => realpath(__DIR__ . '/../'),
+        'defaultRoute' => 'home/index'
+//        'components' => [
+//            'request' => [
+//                'cookieValidationKey' => 'super secret code'
+//            ],
+//        ]
+    ];
--- web/index.php
+++ web/index.php
@@ -1,1 +1,10 @@
-<h1>Hello School!</h1>
+<?php
+
+define ('YII_DEBUG', true);
+require (__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+$config = require (__DIR__ . '/../config/web.php');
+
+$app = new yii\web\Application($config);
+$app -> run();
+
+?>
